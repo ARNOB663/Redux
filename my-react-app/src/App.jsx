f9133510f1bd8c6b579381a2948647cad697f9b3
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-06-30 21:12:05 +0600

    done with basic redux with the countr project

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,30 @@
 import './App.css'
 
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './redux/features/counter/counterSlice';
+
+
 function App() {
   
+const dispatch = useDispatch();
+const {count} = useSelector((state) => state.counter);
+
+
+const handleIncrement = (amount) =>{
+  dispatch(increment(amount))
+}
+const handleDecrement = (amount) => {3
+  dispatch(decrement(amount))
+}
 
   return (
    <div>
       <h1>Counter with Redux</h1>
-      <button>Increment</button>
-       <button>Decrement</button>
-      <div>0</div>
-
+      <button onClick={()=>handleIncrement(1)}>Increment</button>
+       <button onClick={()=>handleIncrement(5)}>Increment by 5</button>
+      <div>{count}</div>
+       <button onClick={()=>handleDecrement(1)}>Decrement</button>
+      
    </div>
   )
 }
