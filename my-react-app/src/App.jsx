0fd3c50006d5481dfbf0ccb384fc543a6c3cc2cd
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-06-30 19:01:07 +0600

    add store for redux

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+      <h1>Counter with Redux</h1>
+      <button>Increment</button>
+       <button>Decrement</button>
+      <div>0</div>
+
+   </div>
+  )
+}
+
+export default App
