6b8dba49fea71ceea02e310250663602f79971cd
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-07-01 15:05:41 +0600

    project setup shadcn install

---
+++ to-do-app/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Basic to do app</h1>
+        <Button>Button</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
